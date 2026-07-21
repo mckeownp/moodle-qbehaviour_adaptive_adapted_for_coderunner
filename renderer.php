b8f0e6554d496ad6e20c6a33dc00f619ba4a12be
Paul McKeown <paul.mckeown@canterbury.ac.nz>
2026-07-21 18:41:14 +1200

    Changed Precheck button so that scroll position is saved after clicking it.

--- renderer.php
+++ renderer.php
@@ -69,6 +69,7 @@ class qbehaviour_adaptive_adapted_for_coderunner_renderer extends qbehaviour_ada
             'name' => $qa->get_behaviour_field_name('precheck'),
             'value' => get_string('precheck', 'qbehaviour_adaptive_adapted_for_coderunner'),
             'class' => 'submit btn btn-secondary',
+            'data-savescrollposition' => 'true',
         );
         if ($options->readonly) {
             $attributes['disabled'] = 'disabled';
